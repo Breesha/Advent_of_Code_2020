bffd943164b24003f664e09a437fe6ae07b5cd11
Breesha Foxton <breeshafoxton@hotmail.com>
2021-04-01 17:17:12 +0100

    Day 6 added, part 1 complete

---
+++ Advent_Day6/Advent_Day6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Advent_Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader readFile = new StreamReader(@"C:\Users\User\Desktop\SPARTA\TRAINING\Advent_of_Code_2020\Advent_Day6\input.txt");
+            string Data = readFile.ReadToEnd();
+            string[] groups = Data.Split( "\n\n");
+            int total = 0;
+
+            foreach (var group in groups)
+            {
+                Console.WriteLine(group);
+                string oneLine = group.Replace("\n", "");
+                Console.WriteLine(oneLine);
+                string removaldup = "";
+                foreach (char c in oneLine)
+                {
+                    if (!removaldup.Contains(c))
+                    {
+                        removaldup += c;
+                    }
+                }
+                Console.WriteLine(removaldup);
+                Console.WriteLine(removaldup.Length);
+                total += removaldup.Length;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
