2587b77c5c4c001f4c5c39dcdbd27137700f0dff
Breesha Foxton <breeshafoxton@hotmail.com>
2021-04-06 17:34:38 +0100

    Day 7 part 1 started

---
+++ Advent_Day7/Advent_Day7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Advent_Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader readFile = new StreamReader(@"C:\Users\User\Desktop\SPARTA\TRAINING\Advent_of_Code_2020\Advent_Day7\input.txt");
+            string Data = readFile.ReadToEnd();
+            string[] groups = Data.Split("\n");
+
+            Console.WriteLine(groups[0]);
+        }
+    }
+}
